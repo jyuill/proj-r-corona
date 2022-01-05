16c46bc6c39c9f5e529c97b96a5c89cb1eb8db32
John Yuill <john@fig4.com>
2022-01-05 12:30:15 -0800

    more efficient solution to insert records in database. still not perfect, but works.

--- covid-canada-database.R
+++ covid-canada-database.R
@@ -4,10 +4,11 @@ library(tidyverse)
 library(RMariaDB)
 library(keyring)
 library(here)
+library(glue)
 
 ## make connection to database
 con4 <- dbConnect(RMariaDB::MariaDB(), user=,keyring::key_list('starter2')[1,2], password=keyring::key_get('starter2'), dbname='starter2')
-dbExecute(con4, "SELECT * FROM starter2.covid_test;")
+dbGetQuery(con4, "SELECT * FROM starter2.covid_test;")
 
 file_load <- here('data','covid-canada-upload.csv')
 ## didn't work due to file access/upload permissions - only some (none?) locations allowed
@@ -56,3 +57,73 @@ for(r in 2:4){
 cc <- read_csv(here('data','covid-canada.csv'))
 ccu <- cc %>% select(-update)
 write_csv(ccu, 'data/covid-canada-upload.csv')
+
+## build query same as above - for complete ccu cols
+for(r in 1:nrow(ccu)){
+  vc1 <- ccu[[r,1]]
+  vc2 <- ccu[[r,2]]
+  vc3 <- ccu[[r,3]]
+  vc4 <- ccu[[r,4]]
+  vc5 <- ccu[[r,5]]
+  vc6 <- ccu[[r,6]]
+  vc7 <- ccu[[r,7]]
+  vc8 <- ccu[[r,8]]
+  vc9 <- ccu[[r,9]]
+  vc10 <- ccu[[r,10]]
+  vc11 <- ccu[[r,11]]
+  vc12 <- ccu[[r,12]]
+  vc13 <- ccu[[r,13]]
+  vc14 <- ccu[[r,14]]
+  vc15 <- ccu[[r,15]]
+  vc16 <- ccu[[r,16]]
+  vc17 <- ccu[[r,17]]
+  vc18 <- ccu[[r,18]]
+  vc19 <- ccu[[r,19]]
+  vc20 <- ccu[[r,20]]
+  vc21 <- ccu[[r,21]]
+  vc22 <- ccu[[r,22]]
+  vc23 <- ccu[[r,23]]
+  vc24 <- ccu[[r,24]]
+  vc25 <- ccu[[r,25]]
+  vc26 <- ccu[[r,26]]
+  vc27 <- ccu[[r,27]]
+  vc28 <- ccu[[r,28]]
+  vc29 <- ccu[[r,29]]
+  vc30 <- ccu[[r,30]]
+  vc31 <- ccu[[r,31]]
+  vc32 <- ccu[[r,32]]
+  vc33 <- ccu[[r,33]]
+  vc34 <- ccu[[r,34]]
+  vc35 <- ccu[[r,35]]
+  vc36 <- ccu[[r,36]]
+  vc37 <- ccu[[r,37]]
+  vc38 <- ccu[[r,38]]
+  vc39 <- ccu[[r,39]]
+ 
+  ## alternate
+## get list of cols to use in query
+fields <- paste0(names(ccu[1:11]), collapse=", ")
+## use glue to get list in form that can be used for query values
+fields2 <- paste0("('{",paste0(names(ccu[1:11]), collapse="}', '{"),"}')")
+vals <- ccu[9,1:11] %>% glue_data(fields2)
+## replace 'NA' with NULL for query
+vals <- vals %>% str_replace_all('NA',"x NULL x")
+vals <- vals %>% str_remove_all("'x ")
+vals <- vals %>% str_remove_all(" x'")
+  ## INSERT to database table - one row at a time :(
+  dbExecute(con4, paste0("INSERT INTO covid_test(",fields,")
+          VALUES ", vals))
+}
+## for real
+## get list of cols to use in query
+fields <- paste0(names(ccu), collapse=", ")
+## with glue
+fields2 <- paste0("('{",paste0(names(ccu), collapse="}', '{"),"}')")
+vals <- ccu[5:6,] %>% glue_data(fields2)
+
+## INSERT to database table - one row at a time :(
+dbExecute(con4, paste0("INSERT INTO covid_test(",fields,")
+          VALUES ", vals))
+
+## using dplyr SQL - recommended for read, not insert
+ccu_test <- tbl(con4, "covid_test")
